c2200ce28f3483e166b6d2b83f1bd504f5d68d6e
Ipyramid <goldendriver0731@gmail.com>
2023-11-20 09:47:21 -0800

    Simple HTTP Server

---
+++ go.mod
@@ -0,0 +1,11 @@
+module example.com/myproject
+
+go 1.21.3
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
+	rsc.io/quote v1.5.2 // indirect
+	rsc.io/sampler v1.3.0 // indirect
+)
---
+++ src/main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+type Todo struct {
+	Title string
+	Done  bool
+}
+
+type TodoPageData struct {
+	PageTitle string
+	Todos     []Todo
+}
+
+func main() {
+	// db, err := sql.Open("mysql", "root@(127.0.0.1:3306)/test?parseTime=true")
+	// if err != nil {
+	// 	fmt.Println("Error opening database:", err.Error())
+	// 	return
+	// }
+
+	// err = db.Ping()
+	// if err != nil {
+	// 	fmt.Println("Error connecting to database:", err.Error())
+	// 	return
+	// }
+
+	// // query := `
+	// //     CREATE TABLE users (
+	// //         id INT AUTO_INCREMENT,
+	// //         username TEXT NOT NULL,
+	// //         password TEXT NOT NULL,
+	// //         created_at DATETIME,
+	// //         PRIMARY KEY (id)
+	// //     );`
+
+	// // _, err = db.Exec(query)
+	// // if err != nil {
+	// // 	fmt.Println("Error creating table:", err.Error())
+	// // 	return
+	// // }
+
+	// username := "johndoe"
+	// password := "secret"
+	// createdAt := time.Now()
+
+	// result, err := db.Exec(`INSERT INTO users (username, password, created_at) VALUES (?, ?, ?)`, username, password, createdAt)
+	// userID, err := result.LastInsertId()
+
+	// if err != nil {
+	// 	fmt.Println("Error creating table:", err.Error())
+	// } else {
+	// 	fmt.Println("index", userID)
+	// }
+	// err = db.Close()
+	// if err != nil {
+	// 	fmt.Println("Error closing database:", err.Error())
+	// 	return
+	// }
+
+	//router
+	r := mux.NewRouter()
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+	})
+
+	r.HandleFunc("/", queryParamHandler)
+	r.HandleFunc("/login", postRequestHandler)
+	r.HandleFunc("/about", aboutHandler)
+	r.HandleFunc("/contact", contactHandler)
+
+	//run server with specific PORT
+	http.ListenAndServe(":8080", r)
+
+}
+
+func queryParamHandler(w http.ResponseWriter, r *http.Request) {
+	paramValue := r.URL.Query().Get("name")
+	fmt.Fprintf(w, "Query parameter value: %s", paramValue)
+}
+
+func postRequestHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "Failed to parse form data", http.StatusBadRequest)
+			return
+		}
+
+		username := r.PostForm.Get("username")
+		email := r.PostForm.Get("email")
+		fmt.Fprintf(w, "POST request. username: %s, email: %s", username, email)
+	}
+}
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "About Request")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Contact Request")
+}
