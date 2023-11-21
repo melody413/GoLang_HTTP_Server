8c17bb51a151d808fb4e512c309f7849235ec0a9
Ipyramid <goldendriver0731@gmail.com>
2023-11-20 18:19:24 -0800

    store data with sql

--- src/main.go
+++ src/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
@@ -19,48 +21,19 @@ type TodoPageData struct {
 }
 
 func main() {
-	// db, err := sql.Open("mysql", "root@(127.0.0.1:3306)/test?parseTime=true")
-	// if err != nil {
-	// 	fmt.Println("Error opening database:", err.Error())
-	// 	return
-	// }
 
-	// err = db.Ping()
-	// if err != nil {
-	// 	fmt.Println("Error connecting to database:", err.Error())
-	// 	return
-	// }
+	// query := `
+	//     CREATE TABLE users (
+	//         id INT AUTO_INCREMENT,
+	//         username TEXT NOT NULL,
+	//         password TEXT NOT NULL,
+	//         created_at DATETIME,
+	//         PRIMARY KEY (id)
+	//     );`
 
-	// // query := `
-	// //     CREATE TABLE users (
-	// //         id INT AUTO_INCREMENT,
-	// //         username TEXT NOT NULL,
-	// //         password TEXT NOT NULL,
-	// //         created_at DATETIME,
-	// //         PRIMARY KEY (id)
-	// //     );`
-
-	// // _, err = db.Exec(query)
-	// // if err != nil {
-	// // 	fmt.Println("Error creating table:", err.Error())
-	// // 	return
-	// // }
-
-	// username := "johndoe"
-	// password := "secret"
-	// createdAt := time.Now()
-
-	// result, err := db.Exec(`INSERT INTO users (username, password, created_at) VALUES (?, ?, ?)`, username, password, createdAt)
-	// userID, err := result.LastInsertId()
-
+	// _, err = db.Exec(query)
 	// if err != nil {
 	// 	fmt.Println("Error creating table:", err.Error())
-	// } else {
-	// 	fmt.Println("index", userID)
-	// }
-	// err = db.Close()
-	// if err != nil {
-	// 	fmt.Println("Error closing database:", err.Error())
 	// 	return
 	// }
 
@@ -97,8 +70,10 @@ func postRequestHandler(w http.ResponseWriter, r *http.Request) {
 		}
 
 		username := r.PostForm.Get("username")
-		email := r.PostForm.Get("email")
-		fmt.Fprintf(w, "POST request. username: %s, email: %s", username, email)
+		password := r.PostForm.Get("password")
+
+		fmt.Fprintf(w, "POST request. username: %s, email: %s", username, password)
+		storeUserInformation(username, password)
 	}
 }
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
@@ -107,4 +82,34 @@ func aboutHandler(w http.ResponseWriter, r *http.Request) {
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Contact Request")
+}
+
+func storeUserInformation(username string, password string) {
+	db, err := sql.Open("mysql", "root@(127.0.0.1:3306)/test?parseTime=true")
+	if err != nil {
+		fmt.Println("Error opening database:", err.Error())
+		return
+	}
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("Error connecting to database:", err.Error())
+		return
+	}
+
+	createdAt := time.Now()
+
+	result, err := db.Exec(`INSERT INTO users (username, password, created_at) VALUES (?, ?, ?)`, username, password, createdAt)
+	userID, err := result.LastInsertId()
+
+	if err != nil {
+		fmt.Println("Error creating table:", err.Error())
+	} else {
+		fmt.Println("index", userID)
+	}
+	err = db.Close()
+	if err != nil {
+		fmt.Println("Error closing database:", err.Error())
+		return
+	}
 }
